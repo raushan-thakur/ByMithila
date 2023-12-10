1029b6c64087ea0aae35ffb06834140eb95cd6ec
Raushan Thakur <raushandtu@gmail.com>
2023-12-11 00:58:40 +0530

    Added Many Files including authController, authHelper,etc.

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,15 +1,15 @@
 import express from "express";
 import {
   registerController,
-  loginController,
-  testController,
-  forgotPasswordController,
-  updateProfileController,
-  getOrdersController,
-  getAllOrdersController,
-  orderStatusController,
+  // loginController,
+  // testController,
+  // forgotPasswordController,
+  // updateProfileController,
+  // getOrdersController,
+  // getAllOrdersController,
+  // orderStatusController,
 } from "../controllers/authController.js";
-import { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
+//mport { isAdmin, requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router();
@@ -18,39 +18,39 @@ const router = express.Router();
 //REGISTER || METHOD POST
 router.post("/register", registerController);
 
-//LOGIN || POST
-router.post("/login", loginController);
+// //LOGIN || POST
+// router.post("/login", loginController);
 
-//Forgot Password || POST
-router.post("/forgot-password", forgotPasswordController);
+// //Forgot Password || POST
+// router.post("/forgot-password", forgotPasswordController);
 
-//test routes
-router.get("/test", requireSignIn, isAdmin, testController);
+// //test routes
+// router.get("/test", requireSignIn, isAdmin, testController);
 
-//protected User route auth
-router.get("/user-auth", requireSignIn, (req, res) => {
-  res.status(200).send({ ok: true });
-});
-//protected Admin route auth
-router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
-  res.status(200).send({ ok: true });
-});
+// //protected User route auth
+// router.get("/user-auth", requireSignIn, (req, res) => {
+//   res.status(200).send({ ok: true });
+// });
+// //protected Admin route auth
+// router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
+//   res.status(200).send({ ok: true });
+// });
 
-//update profile
-router.put("/profile", requireSignIn, updateProfileController);
+// //update profile
+// router.put("/profile", requireSignIn, updateProfileController);
 
-//orders
-router.get("/orders", requireSignIn, getOrdersController);
+// //orders
+// router.get("/orders", requireSignIn, getOrdersController);
 
-//all orders
-router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
+// //all orders
+// router.get("/all-orders", requireSignIn, isAdmin, getAllOrdersController);
 
-// order status update
-router.put(
-  "/order-status/:orderId",
-  requireSignIn,
-  isAdmin,
-  orderStatusController
-);
+// // order status update
+// router.put(
+//   "/order-status/:orderId",
+//   requireSignIn,
+//   isAdmin,
+//   orderStatusController
+// );
 
 export default router;
