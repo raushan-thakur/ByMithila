a63a63bddf8de8c82c745d21e55c95607997099f
rrohann24 <rastogirohan.a@gmail.com>
2023-12-25 18:44:57 +0530

    worked on frontend

---
+++ client/src/components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from './Header'
+import Footer from './Footer'
+const Layout = ({children}) => {
+  return (
+    <div>
+        <Header/>
+        <main style = {{minHeight:"69vh"}}>
+        {children}
+        </main>
+        <Footer/>
+    </div>
+  )
+}
+
+export default Layout
