f938dfc8fe9c91472489788e27e3ba57b22c29f3
rrohann24 <rastogirohan.a@gmail.com>
2024-02-23 13:10:18 +0530

    issue resolved

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ import categoryRoutes from "./routes/categoryRoutes.js";
 import productsRoutes from "./routes/productsRoutes.js";
 import cors from "cors";
 import path from 'path';
+import { fileURLToPath } from "url";
 //configure env
 dotenv.config();
 
 //databse config
 connectDB();
-
+//esmodule fix
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 //rest object
 const app = express();
 
