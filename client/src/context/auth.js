467ad5cede01bb9c7907c25f677f0ec8c99c7a54
Raushan Thakur <126002112+raushan-thakur@users.noreply.github.com>
2025-11-28 19:06:17 +0530

    Refactor AuthProvider for cleaner code structure

--- client/src/context/auth.js
+++ client/src/context/auth.js
@@ -1,6 +1,6 @@
 import { useState, useEffect, useContext, createContext } from "react";
 import axios from "axios";
-import { token } from "morgan";
+
 const AuthContext = createContext();
 
 const AuthProvider = ({ children }) => {
@@ -8,29 +8,30 @@ const AuthProvider = ({ children }) => {
     user: null,
     token: "",
   });
-
-  //default axios
-  axios.defaults.headers.common['Authorization']=auth?.token;
+  
+  // default axios
+  axios.defaults.headers.common["Authorization"] = auth?.token;
+  
   useEffect(() => {
     const data = localStorage.getItem("auth");
-    if (data) {
-      const parseData = JSON.parse(data);
-      setAuth({
-        ...auth,
-        user: parseData.user,
-        token: parseData.token,
-      });
+      if (data) {
+        const parseData = JSON.parse(data);
+        setAuth({
+          ...auth,
+          user: parseData.user,
+          token: parseData.token,
+        });
     }
-     //eslint-disable-next-line
+    // eslint-disable-next-line
   }, []);
+  
   return (
-    <AuthContext.Provider value={[auth, setAuth]}>
-      {children}
-    </AuthContext.Provider>
+  <AuthContext.Provider value={[auth, setAuth]}>
+    {children}
+  </AuthContext.Provider>
   );
 };
 
-// custom hook
 const useAuth = () => useContext(AuthContext);
 
 export { useAuth, AuthProvider };
